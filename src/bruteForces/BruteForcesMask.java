6f7b04cbf64a1af47b420f003959d379d0b90fdd
Jack <jahon.080879@gmail.com>
2021-10-19 20:32:00 +0500

    Init project

---
+++ src/bruteForces/BruteForcesMask.java
@@ -0,0 +1,34 @@
+package bruteForces;
+
+import java.util.Scanner;
+
+/**
+ * @author Sabirov Jakhongir
+ */
+public class BruteForcesMask {
+    /**
+     * Bizda n ta buyum bor. Biz ulardan tanlash amalga oshirganimzida har bir buyum uchun ikkita variant bor, tanlangan to’plamga tegishliligi yoki tegishli emasligi. n ta razriyadli ikkilik sanoq sistemasida 0 dan 2n-1 gacha bo’lgan sonlarni ikkilikdagi kodida mumkin bo’lgan barcha kombinatsiyalar ko’riladi.
+     * Masalan n=3 bo’lsa 0 dan 23-1 gacha sonlarning ikkilik kodlari:
+     * 0-000    4-100
+     * 1-001    5-101
+     * 2-010    6-110
+     * 3-011    7-111
+     * Har bir kombinatsiyada sonning ikkilik kodidagi mos belgisi 1 ga teng bo’lsa uni tegishli, 0 ga teng bo’lsa tegishli emas deb olamiz.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        int X = cin.nextInt();
+        int a[] = new int[n];
+        for(int i = 0 ; i < n; i ++){
+            a[i] = cin.nextInt();
+        }
+        for(int mask = 0 ; mask < (1 << n) ;mask ++){
+
+        }
+
+
+
+    }
+}
