d8ea2ac0f83a3b482c428e1d87d42637d51d6def
Sabirov Jakhongir <jahon.080879@gmail.com>
2021-11-15 09:27:50 +0500

    DFS algorithm

---
+++ src/com/company/Graph.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
+
+/*
+ *
+ *  @author Sabirov Jakhongir
+ *
+ */
+public class Graph {
+    // This example of dfs algorithm that implemented in java with recursion
+    static LinkedList<Integer> adj[];
+    static boolean used[] = new boolean[10001];
+    static void dfs(int v){
+
+        System.out.println("vertex in " + v);
+        used[v] = true;
+        for(int i = 0  ; i < adj[v].size(); i ++){
+            int to = adj[v].get(i);
+            if(!used[to]){
+                dfs(to);
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n , m, v1, v2;
+        n = cin.nextInt();
+        m = cin.nextInt();
+        adj = new LinkedList[n + 1];
+        for(int i = 0 ; i <= n ;i ++){
+          adj[i] = new LinkedList<>();
+        }
+        for(int i = 1 ; i <= m;i ++){
+            v1 = cin.nextInt();
+            v2 = cin.nextInt();
+            adj[v1].add(v2);
+            adj[v2].add(v1);
+        }
+        int v;
+        v = cin.nextInt();
+        dfs(v);
+
+
+    }
+}
