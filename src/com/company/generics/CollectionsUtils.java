985f6a5be9145d1505e15d6b18ecf323c5b94a42
Jack <jahon.080879@gmail.com>
2021-11-20 15:47:41 +0500

    Write comparing object using with lambda and simple one

--- src/com/company/generics/CollectionsUtils.java
+++ src/com/company/generics/CollectionsUtils.java
@@ -1,5 +1,6 @@
 package com.company.generics;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -21,6 +22,29 @@ public class CollectionsUtils {
     }
 
     public static void main(String[] args) {
+        List<Person> list1 = Arrays.asList(
+                new Person("Aaa",1),
+                new Person("C",12),
+                new Person("Bdd",10)
+        );
+
+        Person mx = max(list1, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                int i = o1.getAge().compareTo(o2.getAge());
+                if(i != 0) return i;
+                return Integer.compare(o1.getAge(),o2.getAge());
+            }
+        });
+        System.out.println(mx.toString());
+
+        // This is in java 8 using java lambda it looks like this
+        Person mx8 = max(list1,Comparator.comparing(Person::getName)
+                .thenComparing(Person::getAge));
+
+
+        System.out.println(mx8.toString());
+
          List<Integer> list = Arrays.asList(15,25,789798);
          Integer m =  max(list, new Comparator<Integer>() {
             @Override
