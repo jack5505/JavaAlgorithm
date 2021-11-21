2c385584ecd4a286568d89ab3bc9e5ce89eaaaa9
Jack <jahon.080879@gmail.com>
2021-11-21 20:41:33 +0500

    Java lambda

--- src/com/company/lambda/Java8Lambda.java
+++ src/com/company/lambda/Java8Lambda.java
@@ -1,6 +1,8 @@
 package com.company.lambda;
 
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntFunction;
+import java.util.function.IntUnaryOperator;
 
 /**
  * @author Sabirov Jakhongir
@@ -14,9 +16,18 @@ public class Java8Lambda {
         return (z, y) -> sum(x,y,z);
     };
 
+    public static IntFunction<IntFunction<IntUnaryOperator>> carry(){
+        return x -> y -> z -> sum(x,y,z);
+    }
+
     public static void main(String[] args) {
        IntBinaryOperator intBinaryOperator = binaryOperator(1);
        int suma = intBinaryOperator.applyAsInt(15,25);
         System.out.println(suma);
+
+        IntUnaryOperator apply = carry().apply(15).apply(57);
+        int x = apply.applyAsInt(15);
+        int y = apply.applyAsInt(20);
+        System.out.println(x + " " + y);
     }
 }
