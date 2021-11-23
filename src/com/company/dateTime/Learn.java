332f444c63e2c9e4d6431c69fca26f4cdeaaa534
Sabirov Jakhongir <jahon.080879@gmail.com>
2021-11-23 10:30:43 +0500

    LocalDateTime

--- src/com/company/dateTime/Learn.java
+++ src/com/company/dateTime/Learn.java
@@ -1,7 +1,9 @@
 package com.company.dateTime;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Month;
+import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 
 /*
@@ -15,6 +17,13 @@ public class Learn {
         LocalDate payday = today.with(TemporalAdjusters.lastDayOfMonth()).minusDays(2);
         System.out.println(today);
         System.out.println(payday);
+
+
+        LocalDateTime todayWithTime = LocalDateTime.now();
+        LocalDateTime time1 = todayWithTime.plusDays(30);
+        System.out.println(time1);
+        LocalDateTime time = todayWithTime.with(TemporalAdjusters.lastDayOfMonth()).plusDays(1);
+        System.out.println(time);
 
         //Immutable
         //Most of the classes in the Date-Time API create objects that are immutable, meaning that, after the object is created, it cannot be modified. To alter the value of an immutable object, a new object must be constructed as a modified copy of the original. This also means that the Date-Time API is, by definition, thread-safe. This affects the API in that most of the methods used to create date or time objects are prefixed with of,
